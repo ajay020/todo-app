2f057c6738961e5094713e9af8868d2b9838f293
ajay020 <ajayjarwal44@gmail.com>
2022-02-14 12:00:11 +0530

    gif image added

--- src/redux/task/taskReducer.js
+++ src/redux/task/taskReducer.js
@@ -1,7 +1,12 @@
 import { ADD_TASK, DELETE_TASK, EDIT_TASK } from './taskTypes';
 
 const initialState = {
-    tasks:[]
+    tasks:[
+        {id: "1", title: "running", completed:false},
+        {id: "4", title: "meditation", completed:false},
+        {id: "3", title: "Give water to plants", completed:false},
+        {id: "5", title: "read for 15 minutes", completed:false},
+    ]
 }
 
 const taskReducer = (state=initialState, action) =>{
